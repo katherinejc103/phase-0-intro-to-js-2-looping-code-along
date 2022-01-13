433c8b637bc9e81447139dc2005762c85dbefad0
Katherine Jean-Charles <katherinejc103@gmail.com>
2022-01-13 14:44:01 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const array= []
+function writeCards(recepiants, feeling) {
+    console.log("recepiants", recepiants);
+    console.log(feeling);
+    
+    for (let i = 0; i < recepiants.length; i++) {
+    array.push(`Thank you, ${recepiants[i]}, for the wonderful ${feeling} gift!`);
+    debugger;
+    }
+        return array;
+}
+
+function countDown(i) {
+    while (i > 0) {
+        console.log(i);
+        i--;
+    }
+ console.log(i)
+}
